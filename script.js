98748681f8b3fc3ce781c841501fea2e09d3e124
Andrei Scafi <andreiscafi@hotmail.com>
2024-03-06 08:53:59 -0300

    Adding range function with javascript.

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 //Functions
+const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
+
+console.log(range(1, 5));
+
 window.onload = () => {
     const container = document.getElementById('container');
     const createLabel = (name) => {
-
+        const label = document.createElement('div');
+        label.className = 'label';
+        label.textContent = name;
+        container.appendChild(label);
     };
 };
 //End of Functions
